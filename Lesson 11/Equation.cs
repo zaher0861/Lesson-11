d9eef90943bf1380bf20fb399643aad39c5e373d
Александр Захаров <zaher0861@yandex.ru>
2022-01-04 19:33:40 +0300

    Добавьте файлы проекта.

---
+++ Lesson 11/Equation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    struct Equation
+    {
+        double k;
+        double b;
+
+        public Equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public void Root()
+        {
+            Console.WriteLine("0 = {0}*x-{1}", k, b);
+            double x = b / k;
+            Console.WriteLine("x = {0:f3}", x);
+        }
+    }
+}
---
+++ Lesson 11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число k:");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Equation equation = new Equation(k, b);
+            equation.Root();
+            Console.ReadKey();
+        }
+    }
+
+
+}
